5a63d1022ff06b710b27670d382c002fc7b77316
Martin Koistinen <mkoistinen@gmail.com>
2015-12-02 16:19:16 -0500

    Adds tox/travis config for flake8 testing (only)

--- setup.py
+++ setup.py
@@ -25,7 +25,11 @@ setup(
     author='Divio AG',
     author_email='info@divio.ch',
     url='https://github.com/divio/djangocms-file',
-    packages=['djangocms_file', 'djangocms_file.migrations', 'djangocms_file.migrations_django'],
+    packages=[
+        'djangocms_file',
+        'djangocms_file.migrations',
+        'djangocms_file.migrations_django'
+    ],
     license='LICENSE.txt',
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
