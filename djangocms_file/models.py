1659a4f876eaa6bc0e87f4a40a64d53039b065b1
Angelo Dini <angelo.dini@divio.ch>
2019-01-15 17:08:33 +0100

    add unicode_literals import

--- djangocms_file/models.py
+++ djangocms_file/models.py
@@ -3,6 +3,8 @@
 Enables the user to add a "File" plugin that displays a file wrapped by
 an <anchor> tag.
 """
+from __future__ import unicode_literals
+
 from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
