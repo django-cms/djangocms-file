53302c3cbb45aa9d9f2a37c765cac9a3fc1535d0
Angelo Dini <angelo.dini@divio.ch>
2020-01-16 15:53:36 +0100

    add django 3 compatibility

--- djangocms_file/models.py
+++ djangocms_file/models.py
@@ -7,9 +7,10 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext
 from django.utils.translation import ugettext_lazy as _
+
+from six import python_2_unicode_compatible
 
 from cms.models import CMSPlugin
 
