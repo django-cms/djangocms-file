cbedfa0de79b9b93f4f35c42691ce5f6d2fb6cbf
Angelo Dini <angelo.dini@divio.ch>
2020-01-29 14:30:43 +0100

    Added support for Django 3 (#43)
    * add django 3 compatibility
    
    * use cms helper
    
    * pin correct version
    
    * use 3.7 as reference
    
    * pin formtools on Django 1.11
    
    * fix
    
    * added changelog entry

--- djangocms_file/models.py
+++ djangocms_file/models.py
@@ -7,7 +7,6 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext
 from django.utils.translation import ugettext_lazy as _
 
@@ -16,6 +15,7 @@ from cms.models import CMSPlugin
 from djangocms_attributes_field.fields import AttributesField
 from filer.fields.file import FilerFileField
 from filer.fields.folder import FilerFolderField
+from six import python_2_unicode_compatible
 
 
 LINK_TARGET = (
